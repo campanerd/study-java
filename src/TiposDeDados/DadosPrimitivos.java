2e0253e52caf54adfbf2aec5241fd920e1f02a0d
Davi Campaner <campanerdavi@gmail.com>
2026-02-13 00:03:56 -0300

    initial project

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,17 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+    /*
+    Dados primitivos - int, double, float, char, boolean, short
+    objetivo da aula: criar um ninja - Naruto
+     */
+
+        int idade = 16;
+        double altura = 1.75;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 99999L; //valor para long é com limite mto maior - sempre escrever com L maiúsculo
+    }
+}
