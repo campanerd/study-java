5fbf210064c134f7b79c8ae1990c01e0b7a842b7
Davi Campaner <campanerdavi@gmail.com>
2026-02-14 01:19:26 -0300

    learning to use sout and soutv

--- src/TiposDeDados/DadosPrimitivos.java
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -13,5 +13,11 @@ public class DadosPrimitivos {
         char inicial = 'N';
         boolean vivoOuMorto = true;
         Long saldoBancario = 99999L; //valor para long é com limite mto maior - sempre escrever com L maiúsculo
+
+        System.out.println(idade); // comando para mostrar o usuário
+        System.out.println(saldoBancario);
+        System.out.println(inicial);
+        System.out.println("saldo Bancario é = " + saldoBancario);
+        System.out.println("Minha idade é: " + idade);
     }
 }
