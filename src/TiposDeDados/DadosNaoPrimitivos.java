d142ed837837c400270d9e648847f4d6ccc59da9
Davi Campaner <campanerdavi@gmail.com>
2026-02-19 22:32:24 -0300

    studying non-primitive data type

--- src/TiposDeDados/DadosNaoPrimitivos.java
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -2,6 +2,24 @@ package TiposDeDados;
 
 public class DadosNaoPrimitivos {
     static void main() {
+    // tudo que for digitado aqui dentro com o comando PSVM vai ser compilado pelo java
+
+        /*
+        *dados nao primitivos: String, Array, Class
+        * objetivo: criar um ninja, e atribuir metodos a ele
+         */
+
+        String nome = "Naruto Uzumaki";
+        String nomeCapsLk = nome.toUpperCase(); // toUpperCase vai colocar tudo em capsLock
+        System.out.println("Esse texto ta em capslk: " + nomeCapsLk);
+        System.out.println("Esse texto ta normal: " + nome);
+
+        String aldeia = "ALdeIa DA FOlha";
+        String aldeiamini = aldeia.toLowerCase(); // toLowerCase vai colocar tudo em minusculo
+        System.out.println(aldeiamini);
+
+
+
 
     }
 }
