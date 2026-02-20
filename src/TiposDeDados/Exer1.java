220b8f76819e2f10a53c04e2e6f0f4166ea3eb5b
Davi Campaner <campanerdavi@gmail.com>
2026-02-19 22:57:17 -0300

    resolve exercise 1

---
+++ src/TiposDeDados/Exer1.java
@@ -0,0 +1,37 @@
+package TiposDeDados;
+
+public class Exer1 {
+    static void main() {
+        //ninja1
+        String ninja1nome = "Sasuke Uchiha";
+        int ninja1idade = 16;
+        String missao = "encontrar o itachi";
+        String statusdaMissao = "Em andamento";
+        char inicial = 'S';
+
+        //ninja2
+        String ninja2nome = "Sakura Haruno";
+        int ninja2idade = 18;
+        String missao2 = "tentar ser util";
+        String statusdaMissao2 = "Incompleta";
+        char inicial2 = 'A';
+
+        //ninja3
+        String ninja3nome = "Minato Namikaze";
+        int ninja3idade = 27;
+        String missao3 = "Selar Kurama";
+        String statusdaMissao3 = "Completa";
+        char inicial3 = 'S';
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
