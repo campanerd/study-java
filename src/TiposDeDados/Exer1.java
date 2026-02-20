31b284cf95b917dd3d67d5c5297d4bd39a28f047
Davi Campaner <campanerdavi@gmail.com>
2026-02-19 23:18:21 -0300

    add if/else logic for ninja1

--- src/TiposDeDados/Exer1.java
+++ src/TiposDeDados/Exer1.java
@@ -4,8 +4,8 @@ public class Exer1 {
     static void main() {
         //ninja1
         String ninja1nome = "Sasuke Uchiha";
-        int ninja1idade = 16;
-        String missao1 = "encontrar o itachi";
+        int ninja1idade = 14;
+        String missao1 = "matar o Itachi";
         String statusdaMissao1 = "Em andamento";
         char nivelmissao1 = 'S';
         System.out.println("Nome do Ninja1: " + ninja1nome);
@@ -13,6 +13,26 @@ public class Exer1 {
         System.out.println("Missão do Ninja1: " + missao1);
         System.out.println("Status da Missão do Ninja1: " + statusdaMissao1);
         System.out.println("Nível da Missão do Ninja1: " + nivelmissao1);
+        System.out.println("----------------------------------------------");
+        System.out.println("----------------------------------------------");
+
+
+        // verificar nivel da missao e idade do ninja
+        if (ninja1idade < 15) {
+            if (nivelmissao1 == 'C' || nivelmissao1 == 'D') {
+                statusdaMissao1 = "Concluida";
+            } else {
+                statusdaMissao1 = "Não concluída";
+            }
+
+
+        }else {
+            statusdaMissao1 = "Concluída";
+        }
+        System.out.println(statusdaMissao1);
+        System.out.println("----------------------------------------------");
+        System.out.println("----------------------------------------------");
+
 
         //ninja2
         String ninja2nome = "Sakura Haruno";
@@ -20,6 +40,14 @@ public class Exer1 {
         String missao2 = "tentar ser util";
         String statusdaMissao2 = "Incompleta";
         char nivelmissao2 = 'A';
+        System.out.println("Nome do Ninja2: " + ninja2nome);
+        System.out.println("Idade da " + ninja2nome +" : " + ninja2idade);
+        System.out.println("Missão do Ninja2: " + missao2);
+        System.out.println("Status da Missão do Ninja2: " + statusdaMissao2);
+        System.out.println("Nível da Missão do Ninja2: " + nivelmissao2);
+        System.out.println("----------------------------------------------");
+        System.out.println("----------------------------------------------");
+
 
         //ninja3
         String ninja3nome = "Minato Namikaze";
@@ -27,6 +55,13 @@ public class Exer1 {
         String missao3 = "Selar Kurama";
         String statusdaMissao3 = "Completa";
         char nivelmissao3 = 'S';
+        System.out.println("Nome do Ninja3: " + ninja3nome);
+        System.out.println("Idade do " + ninja3nome +" : " + ninja3idade);
+        System.out.println("Missão do Ninja3: " + missao3);
+        System.out.println("Status da Missão do Ninja3: " + statusdaMissao3);
+        System.out.println("Nível da Missão do Ninja3: " + nivelmissao3);
+        System.out.println("----------------------------------------------");
+        System.out.println("----------------------------------------------");
 
 
 
