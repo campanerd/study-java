f81fb0fc64f01ddd2d20cebd3744979fe51ca9ce
Davi Campaner <campanerdavi@gmail.com>
2026-02-19 23:03:22 -0300

    print ninja1 info

--- src/TiposDeDados/Exer1.java
+++ src/TiposDeDados/Exer1.java
@@ -5,23 +5,29 @@ public class Exer1 {
         //ninja1
         String ninja1nome = "Sasuke Uchiha";
         int ninja1idade = 16;
-        String missao = "encontrar o itachi";
-        String statusdaMissao = "Em andamento";
-        char inicial = 'S';
+        String missao1 = "encontrar o itachi";
+        String statusdaMissao1 = "Em andamento";
+        char nivelmissao1 = 'S';
+        System.out.println("Nome do Ninja1: " + ninja1nome);
+        System.out.println("Idade do " + ninja1nome +" : " + ninja1idade);
+        System.out.println("Missão do Ninja1: " + missao1);
+        System.out.println("Status da Missão do Ninja1: " + statusdaMissao1);
+        System.out.println("Nível da Missão do Ninja1: " + nivelmissao1);
 
         //ninja2
         String ninja2nome = "Sakura Haruno";
         int ninja2idade = 18;
         String missao2 = "tentar ser util";
         String statusdaMissao2 = "Incompleta";
-        char inicial2 = 'A';
+        char nivelmissao2 = 'A';
 
         //ninja3
         String ninja3nome = "Minato Namikaze";
         int ninja3idade = 27;
         String missao3 = "Selar Kurama";
         String statusdaMissao3 = "Completa";
-        char inicial3 = 'S';
+        char nivelmissao3 = 'S';
+
 
 
 
